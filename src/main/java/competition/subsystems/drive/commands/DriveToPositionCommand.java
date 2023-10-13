7ddf51c24978cabe8dbedbd519023cc8a1fb3a05
kujo27 <johnkuang783@gmail.com>
2023-10-13 11:34:21 -0700

    done

--- src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
@@ -28,7 +28,12 @@ public class DriveToPositionCommand extends BaseCommand {
     public void setTargetPosition(double position) {
         // This method will be called by the test, and will give you a goal distance.
         // You'll need to remember this target position and use it in your calculations.
+        
+        
+        
+        //assigning the target position to a variable "goal"
         goal = position;
+    
     }
 
     @Override
@@ -46,17 +51,22 @@ public class DriveToPositionCommand extends BaseCommand {
 
         // How you do this is up to you. If you get stuck, ask a mentor or student for
         // some hints!
+        
+        
+        
+        
+        //setting the robots current position to a variable "currentPose"
         currentPose = pose.getPosition();
         
-        
-        
+        //error is distance from goal and robots current position
         error = goal - currentPose;
+        //asssinging power to robot that scales down when the error 
         power = error * 1.35 - velocity * 5.6;
         drive.tankDrive(power, power);
+        // assigning the distance between the old position with current position as velocity (distance over time)
         velocity = currentPose - oldPose;
+        //assigning current position as old position so robot positions keep updating
         oldPose = currentPose;
-    
-        error = goal - currentPose;
     }
         
 
@@ -64,9 +74,16 @@ public class DriveToPositionCommand extends BaseCommand {
     public boolean isFinished() {
         // Modify this to return true once you have met your goal,
         // and you're moving fairly slowly (ideally stopped)
+        
+        //updating the velocity and error variables 
         double velocity = currentPose - oldPose;
         double error = goal - currentPose;
-        if (error < 0.1 && error > -0.1 && velocity < 0.01 && velocity > -0.01) {
+        
+        //ends the program when error is very little, when robot is basically on the target position
+        
+        
+        // if (error < 0.1 && error > -0.1 && velocity < 0.01 && velocity > -0.01) {
+        if (error < 0.1 && error > -0.1) {
             drive.tankDrive(0,0);
             return true;
 
--- src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -11,6 +11,15 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
     DriveSubsystem drive;
     PoseSubsystem pose;
 
+    double currentAngle;
+    double oldAngle;
+    double goalAngle;
+    double error;
+    double power;
+    double rotation;
+    double startingAngle;
+    
+    
     @Inject
     public TurnLeft90DegreesCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
         this.drive = driveSubsystem;
@@ -19,10 +28,58 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
 
     @Override
     public void initialize() {
+        startingAngle = pose.getCurrentHeading().getDegrees();
+        goalAngle = 90;
     }
 
     @Override
     public void execute() {
+        currentAngle = pose.getCurrentHeading().getDegrees() - startingAngle;
+        
+        // conditional that checks if the angle starts positive and ends up negative.
+        if (pose.getCurrentHeading().getDegrees() < 0 && startingAngle > 0) {
+            //makes the angle positive
+            currentAngle = 360 + pose.getCurrentHeading().getDegrees() - startingAngle;
+        }
+        
+        //error is goal angle - current angle
+        error = goalAngle - currentAngle;
+
+
+        
+        //power assigned to motors is scaling down when error is less (error subtracting rotation)
+        power = error * 0.023 - rotation * 0.43;
+    
+
+        //giving power to motors
+        drive.tankDrive(-power, power);
+        rotation = currentAngle - oldAngle;
+        oldAngle = currentAngle;
     }
+
+    @Override
+    public boolean isFinished() {
+        double error =  goalAngle - currentAngle;
+        double rotation = currentAngle - oldAngle;
+        
+        if (Math.abs(error) < 0.1 && Math.abs(rotation) < 0.1) {
+         
+            drive.tankDrive(0,0);
+            return true;
+        }
+        return false;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
